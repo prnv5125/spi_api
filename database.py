61fc54cf02d536c7e93ae235ca86c8635b2cb8aa
pranav <pranavkanade2022@gmail.com>
2025-11-01 19:52:56 +0530

    Added Supabase connection

--- database.py
+++ database.py
@@ -1,11 +1,8 @@
 from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, declarative_base
 
-# Replace with your own credentials
-DATABASE_URL = "postgresql://postgres:Pranav@localhost:5432/corrugated_db"
+DATABASE_URL = postgresql://postgres:Pranav@spi@db.qfpqrkvovdjzxshfbyuq.supabase.co:5432/postgres
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
 Base = declarative_base()
